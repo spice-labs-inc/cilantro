01906cba1cdb0090415816fe8d2780d22b837fa9
sdh <github@plinth.org>
2025-07-28 09:26:01 -0400

    Refactored to use UUID instead of GUID.

--- src/cilantro/src/main/scala/cilantro.metadata/GuidHeap.scala
+++ src/cilantro/src/main/scala/cilantro.metadata/GuidHeap.scala
@@ -12,14 +12,14 @@
 
 package io.spicelabs.cilantro.metadata
 
-import io.spicelabs.dotnet_support.Guid
+import java.util.UUID
 
 class GuidHeap(data: Array[Byte]) extends Heap(data) {
     def read(index: Int) =
         val guid_size = 16
         if (index == 0 || ((index - 1) + guid_size) > data.length)
-            Guid.empty // this might not be right
+            UUID.randomUUID()
         val buffer = Array.ofDim[Byte](guid_size)
         Array.copy(data, index - 1, buffer, 0, guid_size)
-        Guid(buffer)
+        UUID.nameUUIDFromBytes(buffer)
 }
